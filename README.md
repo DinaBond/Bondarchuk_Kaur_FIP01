ddeff94b04c4740c3baf8f843a79727d0ec2634b
Dina Bondarchuk <d_bondarchuk@fanshaweonline.ca>
2024-04-14 17:27:51 -0400

    add readme and code
    In this commit code and readme were added.

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Bondarchuk_Kaur_FIP01
 This is a website  for FIP
+
+# Bondarchuk_Dina_Harnoorpret_Kaur_FIP
+This repository represents the final integrated project: rebranding DR.NUT and crating website for this brand.
+
+
+## Installation
+This is no installatin required, but should be opened with live server.
+
+## Usage
+Open index.html in the browser of your choice.
+
+## Contributing
+1. Fork it!
+2. Create your feature branch: git checkout -b my-new-feature
+3. Commit your changes: git commit -am 'Add some feature'
+4. Push to the branch: git push origin my-new-feature
+5. Submit a pull request :D
+
+## History
+April 14, 2024
+
+## Credits
+Dina Bondarchuk
+Harnoorpreet Kaur
+
+## License
+MIT License
--- js/main.js
+++ js/main.js
@@ -30,11 +30,15 @@
 //(() => {  })();   
 
 
-//video 
-let video = document.getElementById('main-video');
+//Video 
+document.addEventListener('DOMContentLoaded', function() {
+    "use strict";
 
-video.addEventListener('play', function() {
-		video.play();
+    let video = document.getElementById('videoPlayer');
+
+    video.addEventListener('loadedmetadata', function() {
+        video.play();
+    });
 });
 
 
@@ -138,17 +142,3 @@ dropZones.forEach(zone => {
     zone.addEventListener("dragover", dragOver);
     zone.addEventListener("drop", drop);
 });
-
-
-
-window.addEventListener("scroll", function() {
-    const distance = window.scrollY
-    document.querySelector("header").style.transform = `translateY(${distance *
-      1}px)`
-    document.querySelector(
-      ".container"
-    ).style.transform = `translateY(${distance * 0.3}px)`
-    setTimeout(() => {
-      document.querySelector("section h3").classList.add("animate-me")
-    }, 400)
-  })
